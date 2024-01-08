fb558aaecb39be969a2f61d24a0548f5686d7c6c
Avellin Wong <avwong@hellmann.com>
2024-01-08 17:53:49 +0800

    Rename charge mapping model to reference

--- Data/ChargeMappingReference.cs
+++ Data/ChargeMappingReference.cs
@@ -3,14 +3,14 @@ using Amazon.Auth.AccessControlPolicy;
 
 namespace _4PL.Data;
 
-public class ChargeMapping
+public class ChargeMappingReference
 {
     public Guid Id { get; set; }
     public string Other_Charge_Description_Name { get; set; }
     public string Source {  get; set; }
     public string Charge_Description { get; set; }
 
-    public ChargeMapping(string id, string other_Charge_Description_Name, string source, string charge_Description)
+    public ChargeMappingReference(string id, string other_Charge_Description_Name, string source, string charge_Description)
     {
         this.Id = Guid.Parse(id);
         this.Other_Charge_Description_Name = other_Charge_Description_Name;
@@ -18,7 +18,7 @@ public class ChargeMapping
         this.Charge_Description = charge_Description;
     }
 
-    public ChargeMapping()
+    public ChargeMappingReference()
     {
         this.Id = Guid.NewGuid(); 
         this.Other_Charge_Description_Name = "";
@@ -26,7 +26,7 @@ public class ChargeMapping
         this.Charge_Description = "";
     }
 
-    public ChargeMapping(ChargeMapping mapping)
+    public ChargeMappingReference(ChargeMappingReference mapping)
     {
         this.Id = mapping.Id; 
         this.Other_Charge_Description_Name = mapping.Other_Charge_Description_Name;
